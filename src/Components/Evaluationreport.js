eaa7d99e0773da292db9e1a7bc83a1e2c3f6d615
Johnnyprez <hesperjohn11@gmail.com>
2024-11-27 11:35:24 +0800

    Update Evaluationreport.js

--- src/Components/Evaluationreport.js
+++ src/Components/Evaluationreport.js
@@ -7,11 +7,11 @@ import {FileChartColumn} from 'lucide-react';
 const Evaluationreport = () => (
     <div className='evaluationreport-container'>
         <div className="evaluationreport-buttons">
-            <Link to="/admin-dashboard/evaluation-reports" className='subject-evaluationreport-button'>
+            <Link to="/admin-dashboard/evaluation-reports" className='evaluationreport-button'>
                 <FileChartColumn style={{ width: '50px', height: '50px', color: 'black' }} />
                 <p>Faculty Report</p>
             </Link>
-            <Link to="/admin-dashboard/subject-evaluation-report" className='facultydean-evaluationreport-button'>
+            <Link to="/admin-dashboard/subject-evaluation-report" className='evaluationreport-button'>
                 <FileChartColumn style={{ width: '50px', height: '50px', color: 'black' }} />
                 <p>Subjects Report</p>
             </Link>
